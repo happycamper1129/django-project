9130494af4428ff437f57b50bae782a7ef2b5743
happycamper1129 <happycamper.sw@gmail.com>
2009-11-22 01:30:23 -0600

    Fixed case where `SearchQuerySet.more_like_this` would fail when using deferred Models. Thanks to Alex Gaynor for the original patch.

--- tests/solr_tests/tests/solr_backend.py
+++ tests/solr_tests/tests/solr_backend.py
@@ -187,6 +187,8 @@ class FailedSolrSearchBackendTestCase(TestCase):
         settings.HAYSTACK_SOLR_URL = "%s/foo/" % settings.HAYSTACK_SOLR_URL
         cap = CaptureHandler()
         logging.getLogger('haystack').addHandler(cap)
+        import haystack
+        logging.getLogger('haystack').removeHandler(haystack.stream)
         
         # Setup the rest of the bits.
         site = SearchSite()
@@ -212,6 +214,7 @@ class FailedSolrSearchBackendTestCase(TestCase):
         # Restore.
         settings.HAYSTACK_SOLR_URL = old_solr_url
         logging.getLogger('haystack').removeHandler(cap)
+        logging.getLogger('haystack').addHandler(haystack.stream)
 
 
 class LiveSolrSearchQueryTestCase(TestCase):
@@ -532,6 +535,14 @@ class LiveSolrMoreLikeThisTestCase(TestCase):
         alt_mlt_with_models = self.sqs.models(MockModel).more_like_this(MockModel.objects.get(pk=1))
         self.assertEqual(alt_mlt_with_models.count(), 23)
         self.assertEqual([result.pk for result in alt_mlt_with_models], ['6', '14', '4', '10', '22', '5', '3', '12', '2', '23', '18', '19', '13', '7', '15', '21', '9', '20', '16', '17', '8', '11'])
+        
+        if hasattr(MockModel.objects, 'defer'):
+            # Make sure MLT works with deferred bits.
+            mi = MockModel.objects.defer('foo').get(pk=1)
+            self.assertEqual(mi._deferred, True)
+            deferred = self.sqs.models(MockModel).more_like_this(mi)
+            self.assertEqual(alt_mlt_with_models.count(), 23)
+            self.assertEqual([result.pk for result in alt_mlt_with_models], ['6', '14', '4', '10', '22', '5', '3', '12', '2', '23', '18', '19', '13', '7', '15', '21', '9', '20', '16', '17', '8', '11'])
 
 
 class SolrRoundTripSearchIndex(indexes.RealTimeSearchIndex):
