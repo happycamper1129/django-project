523e5c58766ec04c0be1f946764df0607850e18a
happycamper1129 <happycamper.sw@gmail.com>
2012-11-09 11:56:06 -0500

    Added failing test case for #438.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,52 +1,36 @@
-#!/bin/bash
-
-if [ "$1" == "--help" ]; then
-    echo "Runs the test suite for all backends"
-    echo
-    echo "See docs/running_tests.rst for instructions on installing test"
-    echo "search engine instances"
-    echo
-    echo "Use $0 --with-coverage to execute tests using coverage.py"
-    echo
-    exit 0
-elif [ "$1" == "--with-coverage" ]; then
-    TEST_RUNNER="coverage run --source=$(realpath "$(dirname "$0")/../haystack") -- `which django-admin.py`"
-else
-    TEST_RUNNER=django-admin.py
-fi
-
+#!/bin/sh
 echo "** CORE **"
-$TEST_RUNNER test core --settings=settings
+django-admin.py test core --settings=settings
 echo ""
 
 echo "** DISCOVERY **"
-$TEST_RUNNER test discovery --settings=discovery_settings
+django-admin.py test discovery --settings=discovery_settings
 echo ""
 
 echo "** OVERRIDES **"
-$TEST_RUNNER test overrides --settings=overrides_settings
+django-admin.py test overrides --settings=overrides_settings
 echo ""
 
 echo "** SIMPLE **"
-$TEST_RUNNER test simple_tests --settings=simple_settings
+django-admin.py test simple_tests --settings=simple_settings
 echo ""
 
 echo "** SOLR **"
-$TEST_RUNNER test solr_tests --settings=solr_settings
+django-admin.py test solr_tests --settings=solr_settings
 echo ""
 
 echo "** Elasticsearch **"
-$TEST_RUNNER test elasticsearch_tests --settings=elasticsearch_settings
+django-admin.py test elasticsearch_tests --settings=elasticsearch_settings
 echo ""
 
 echo "** WHOOSH **"
-$TEST_RUNNER test whoosh_tests --settings=whoosh_settings
+django-admin.py test whoosh_tests --settings=whoosh_settings
 echo ""
 
 echo "** MULTIPLE INDEX **"
-$TEST_RUNNER test multipleindex --settings=multipleindex_settings
+django-admin.py test multipleindex --settings=multipleindex_settings
 echo ""
 
 echo "** SPATIAL **"
-$TEST_RUNNER test spatial --settings=spatial_settings
+django-admin.py test spatial --settings=spatial_settings
 echo ""
