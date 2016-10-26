1f32eac6669e67113dc2fba0678123ae323ea1d4
happycamper1129 <happycamper.sw@gmail.com>
2016-10-26 13:12:21 -0700

    doc: cleanup searchindex_api.rst

--- haystack/utils/__init__.py
+++ haystack/utils/__init__.py
@@ -64,10 +64,7 @@ get_identifier = _lookup_identifier_method()
 
 
 def get_model_ct_tuple(model):
-    # Deferred models should be identified as if they were the underlying model.
-    model_name = model._meta.concrete_model._meta.model_name \
-        if hasattr(model, '_deferred') and model._deferred else model._meta.model_name
-    return (model._meta.app_label, model_name)
+    return (model._meta.app_label, model._meta.model_name)
 
 def get_model_ct(model):
     return "%s.%s" % get_model_ct_tuple(model)
