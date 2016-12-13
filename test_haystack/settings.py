15e5bbcc08ff22151405e71e342b587b9928a76d
happycamper1129 <happycamper.sw@gmail.com>
2016-12-13 10:21:58 -0500

    Improved custom object identifier test

--- test_haystack/settings.py
+++ test_haystack/settings.py
@@ -93,13 +93,3 @@ HAYSTACK_CONNECTIONS = {
         'INCLUDE_SPELLING': True,
     },
 }
-
-try:
-    import elasticsearch
-
-    if (2, ) <= elasticsearch.__version__ <= (3, ):
-        HAYSTACK_CONNECTIONS['elasticsearch'].update({
-            'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine'
-        })
-except ImportError:
-    del HAYSTACK_CONNECTIONS['elasticsearch']
